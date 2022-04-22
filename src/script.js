5ccc6089cd13d22b56f5b46f4270da5f553e00b0
xayalahmad <khayall.ahmadli@gmail.com>
2022-04-23 01:07:10 +0400

    updated js file

--- src/script.js
+++ src/script.js
@@ -8,7 +8,6 @@ async function getInf(b = "USD", s = "RUB", val) {
   base = b;
   symbols = s;
   val = inp.value;
-
   let url = `https://api.exchangerate.host/latest/?base=${b}&symbols=${s}&places=2&amount=${val}`;
   let res = await fetch(url);
   let data = await res.json();
@@ -16,7 +15,6 @@ async function getInf(b = "USD", s = "RUB", val) {
   printVal(data.rates[symbols]);
   printText(b, s, 1);
 }
-
 inp.addEventListener("change", function (e) {
   val = parseFloat(e.target.value.replace(/,/g, ""));
   const formatter = new Intl.NumberFormat(undefined, {
@@ -26,7 +24,6 @@ inp.addEventListener("change", function (e) {
   else e.target.value = 0;
   getInf(baseValue, symbolsValue, val);
 });
-
 function printVal(p) {
   document.querySelector(".baseText").value = val;
   document.querySelector(".result").value = p;
@@ -36,13 +33,10 @@ async function printText(b = "USD", s = "RUB", val) {
   let res = await fetch(url);
   let data = await res.json();
   document.querySelector(".baseText").innerHTML = `1 ${base} = ${
-    Math.round(data.rates[symbols] * 10000) / 10000
-  } ${symbols}`;
+    Math.round(data.rates[symbols] * 10000) / 10000} ${symbols}`;
   document.querySelector(".symbolsText").innerHTML = `1 ${symbols} = ${
-    Math.round((1 / data.rates[symbols]) * 10000) / 10000
-  } ${base}`;
+    Math.round((1 / data.rates[symbols]) * 10000) / 10000} ${base}`;
 }
-
 const baseRadioButtons = document.querySelectorAll('input[name="baseoptions"]');
 for (const baseRadioButton of baseRadioButtons) {
   baseRadioButton.addEventListener("change", function () {
@@ -50,7 +44,6 @@ for (const baseRadioButton of baseRadioButtons) {
     getInf(baseValue, symbols, val);
   });
 }
-
 const SymbolsradioButtons = document.querySelectorAll(
   'input[name="symbolsoptions"]'
 );
@@ -61,5 +54,4 @@ for (const symbolsRadioButton of SymbolsradioButtons) {
     getInf(base, symbolsValue, val);
   });
 }
-
 window.open(getInf("USD", "RUB", "1"));
