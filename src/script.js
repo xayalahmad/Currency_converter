7505a35ef05ce214d26c03a623a353fcce93cb61
xayalahmad <khayall.ahmadli@gmail.com>
2022-04-25 22:06:07 +0400

    added second input event

--- src/script.js
+++ src/script.js
@@ -1,101 +1,70 @@
-const inp = document.querySelector(".mynumber");
-const inp2 = document.querySelector(".input2");
-let base;
-let symbols;
-let val;
-let baseValue;
-let symbolsValue;
-let num
-async function getInfInput1(b = "USD", s = "RUB", val) {
-  base = b;
-  symbols = s;
-  val = inp.value;
-  let url = `https://api.exchangerate.host/latest/?base=${b}&symbols=${s}&places=2&amount=${val}`;
-  let res = await fetch(url);
-  let data = await res.json();
-  console.log(data);
-  num = data.rates[symbols];
+const form = document.querySelector(".form");
+const inputBase = document.querySelector(".mynumber");
+const inputTo = document.querySelector(".input2");
 
-  printVal(data.rates[symbols]);
-  printText(b, s, 1);
-}
-async function getInfInput2(b = "USD", s = "RUB", val) {
-  base = s;
-  symbols = b;
-  val = inp2.value;
-  let url = `https://api.exchangerate.host/latest/?base=${s}&symbols=${b}&places=2&amount=${val}`;
-  let res = await fetch(url);
-  let data = await res.json();
-  console.log(data);
-  num = data.rates[symbols];
-  printVal2(data.rates[symbols]);
-  printText(b, s, 1);
-}
-inp.addEventListener("input", function (e) {
-  val = parseFloat(e.target.value.replace(/,/g, ""));
-  // const formatter = new Intl.NumberFormat(undefined, {
-  //   maximumFractionDigits: 4,
-  // });
-  // if (val) e.target.value = formatter.format(val);
-  // else e.target.value = 0;
-  getInfInput1(baseValue, symbolsValue, val);
-});
-inp2.addEventListener("input", function (e) {
-  val = parseFloat(e.target.value.replace(/,/g, ""));
-  // const formatter = new Intl.NumberFormat(undefined, {
-  //   maximumFractionDigits: 4,
-  // });
-  // if (val) e.target.value = formatter.format(val);
-  // else e.target.value = 0;
-  getInfInput2(baseValue, symbolsValue, val);
-});
-function printVal(p) {
-  console.log(val);
-  if(val == ""){
-    console.log(p);
-    p = ""
-    console.log(p);
+const baseCurrencyDiv = document.querySelector(".baseText");
+const toCurrencyDiv = document.querySelector(".symbolsText");
+
+const alert = document.querySelector('.alert');
+
+const currencyMask = {
+  mask: Number,
+  thousandsSeparator: " ",
+  radix: ".",
+  scale: 4,
+  max: Number.MAX_VALUE,
+  padFractionalZeros: false,
+  normalizeZeros: true
+};
+
+const maskBase = IMask(inputBase, currencyMask);
+const maskTo = IMask(inputTo, currencyMask);
+
+async function getData(flag = true) {
+  const myForm = new FormData(form);
+  
+  if(!navigator.onLine) {
+    showAlert('The network connection has been lost.');
+    maskBase.value = '1';
+    return;
   }
-  document.querySelector(".baseText").value = val;
-  document.querySelector(".result").value = p;
-}
-function printVal2(p) {
-  console.log(p);
-  console.log(val);
-  if(val == "NaN"){
-    console.log("ddd");
-    val = " "
-    p = ""
+
+  const base = myForm.get("baseoptions");
+  const symbol = myForm.get("symbolsoptions");
+  let currencyRate ;
+
+  if(base !== symbol) {
+    const url = new URL("/lates", "https://api.exchangerate.host");
+  
+    url.searchParams.set("base", base);
+    url.searchParams.set("symbols", symbol);
+    url.searchParams.set("places", 4);
+  
+    const data = await fetch(url).then((r) => r.json()).catch(e => showAlert('Sorry! We have a problem with API.'));
+    currencyRate = await data.rates[symbol];
   }
-  document.querySelector(".mynumber").value = p;
-  document.querySelector(".result").value = val;
+  else {
+    currencyRate = 1;
+  }
+
+  baseCurrencyDiv.textContent = `1 ${base} = ${currencyRate.toFixed(4)} ${symbol}`;
+  toCurrencyDiv.textContent = `1 ${symbol} = ${(1 / currencyRate).toFixed(4)} ${base}`;
+  
+  if (flag) 
+    maskTo.value = (Math.round(maskBase.unmaskedValue * currencyRate * 10000) / 10000).toString();
+  else 
+    maskBase.value = (Math.round(maskTo.unmaskedValue * (1 / currencyRate) * 10000) / 10000).toString();
 }
-async function printText(b = "USD", s = "RUB", val) {
-  let url = `https://api.exchangerate.host/latest/?base=${b}&symbols=${s}&places=2&amount=${val}`;
-  let res = await fetch(url);
-  let data = await res.json();
-  console.log(data.rates[symbols]);
-  document.querySelector(".baseText").innerHTML = `1 ${base} = ${
-    Math.round(num * 10000) / 10000} ${symbols}`;
-  document.querySelector(".symbolsText").innerHTML = `1 ${symbols} = ${
-    Math.round((1 / num) * 10000) / 10000} ${base}`;
+
+window.addEventListener('load', e => getData());
+
+window.addEventListener("online", () => alert.style.display = 'none');
+
+form.addEventListener("input", e => {
+  e.target.name === "input2" ? getData(false) : getData();
+});
+
+const showAlert = (message) => {
+  alert.textContent = message;
+  alert.style.display = 'block';
 }
-const baseRadioButtons = document.querySelectorAll('input[name="baseoptions"]');
-for (const baseRadioButton of baseRadioButtons) {
-  baseRadioButton.addEventListener("change", function () {
-    baseValue = this.value;
-    getInfInput1(baseValue, symbols, val);
-  });
-}
-const SymbolsradioButtons = document.querySelectorAll(
-  'input[name="symbolsoptions"]'
-);
-for (const symbolsRadioButton of SymbolsradioButtons) {
-  symbolsRadioButton.addEventListener("change", function () {
-    console.log(this.value);
-    symbolsValue = this.value;
-    getInfInput1(base, symbolsValue, val);
-  });
-}
-// window.open(getInfInput2("USD", "RUB", val));
-window.open(getInfInput1("USD", "RUB", "1"));
